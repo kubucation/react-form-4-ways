6420fd98cab1ad65227a65226fc0ffea971ab45f
Etienne Dilocker <etienne@dilocker.de>
2019-01-19 12:05:30 +0100

    implemet controlled form in stateful component
    this correspends to part 2 of the video series

--- src/ControlledForm.js
+++ src/ControlledForm.js
@@ -2,20 +2,56 @@ import React from 'react';
 import Input from './Input';
 import {Link} from 'react-router-dom';
 
-const ControlledForm = () => (
-  <div>
-    <h2>Controlled Form</h2>
-    <form>
-      <Input
-        type="text"
-        label="Customer ID"
-        id="customer-id"
-        name="customer-id"
-      />
-      <button type="submit">Submit</button>
-    </form>
-    <Link to="summary">Conclusions</Link>
-  </div>
-);
+class ControlledForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customerId: '',
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      customerId: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(`The customer id is ${this.state.customerId}`);
+  };
+
+  isValid() {
+    if (this.state.customerId === '') {
+      return false;
+    }
+
+    return true;
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Controlled Form</h2>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            type="text"
+            label="Customer ID"
+            id="customer-id"
+            name="customer-id"
+            onChange={this.handleInputChange}
+            value={this.state.customerId}
+            errorMessage={this.isValid() ? '' : 'This field is required'}
+          />
+          <button disabled={!this.isValid()} type="submit">
+            Submit
+          </button>
+        </form>
+        <Link to="summary">Conclusions</Link>
+      </div>
+    );
+  }
+}
 
 export default ControlledForm;
--- src/summary/ControlledForm.js
+++ src/summary/ControlledForm.js
@@ -3,17 +3,23 @@ import React from 'react';
 const Summary = () => (
   <div>
     <h2>Controlled Form - Conclusions</h2>
-    <h3 className="advatages">Advantages</h3>
+    <h3 className="advantages">Advantages</h3>
     <ul>
-      <li>advantage 1</li>
+      <li>no dependencies (other than React itself)</li>
+      <li>idiomatic react</li>
+      <li>full control, extend it with any state you want</li>
+      <li>"live access", not just on submit</li>
     </ul>
-    <h3 className="disadvatages">Disadvantages</h3>
+    <h3 className="disadvantages">Disadvantages</h3>
     <ul>
-      <li>disadvantage 1</li>
+      <li>quite verbose</li>
+      <li>do we really have to reinvent the wheel</li>
+      <li>what if our forms grow more complex?</li>
     </ul>
     <h3>When should you use this</h3>
     <ul>
-      <li>reason to use</li>
+      <li>simple forms with only basic validation</li>
+      <li>if you don't want to introduce 3rd party dependencies</li>
     </ul>
   </div>
 );
