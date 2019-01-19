ca9e5601cd370155b2b108d5f634866c17b5b941
Etienne Dilocker <etienne@dilocker.de>
2019-01-19 12:06:38 +0100

    implement solution using final form
    this corresponds to part 4 of the video series

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import Input from './Input';
-import {Link} from 'react-router-dom';
+import {Form, Field} from 'react-final-form';
 
-const FinalForm = () => (
-  <div>
-    <h2>Final Form</h2>
-    <form>
-      <Input
-        type="text"
-        label="Customer ID"
-        id="customer-id"
-        name="customer-id"
-      />
-      <button type="submit">Submit</button>
-    </form>
-    <Link to="summary">Conclusions</Link>
-  </div>
+const renderInput = ({input, meta}) => (
+  <Input {...input} type="text" errorMessage={meta.touched && meta.error} />
 );
 
-export default FinalForm;
+const onSubmit = values => {
+  alert(JSON.stringify(values));
+};
 
-// const composeValidators = (...validators) => value =>
-//   validators.reduce((error, validator) => error || validator(value), undefined);
+const required = v => {
+  if (!v || v === '') {
+    return 'This field is required';
+  }
+
+  return undefined;
+};
+
+const allowedNames = v => {
+  if (v === 'forbidden name') {
+    return '"forbidden name" is not a valid customer id';
+  }
+
+  return undefined;
+};
+
+const FinalForm = () => (
+  <Form
+    onSubmit={onSubmit}
+    render={({handleSubmit, invalid}) => (
+      <div>
+        <h2>Final Form</h2>
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="customer=id"
+            component={renderInput}
+            validate={composeValidators(required, allowedNames)}
+          />
+          <button type="submit" disabled={invalid}>
+            Submit
+          </button>
+        </form>
+      </div>
+    )}
+  />
+);
+
+const composeValidators = (...validators) => value =>
+  validators.reduce((error, validator) => error || validator(value), undefined);
+
+export default FinalForm;
--- src/summary/FinalForm.js
+++ src/summary/FinalForm.js
@@ -3,17 +3,25 @@ import React from 'react';
 const Summary = () => (
   <div>
     <h2>Final Form - Conclusions</h2>
-    <h3 className="advatages">Advantages</h3>
+    <h3 className="advantages">Advantages</h3>
     <ul>
-      <li>advantage 1</li>
+      <li>no need to install redux or any "central" component in your app</li>
+      <li>works without previous setup</li>
+      <li>API as good as Redux Form</li>
+      <li>very powerful: validation, dynamic rendering, etc</li>
+      <li>will feel very familiar when you've used Redux Form before</li>
+      <li>very optimizable using subscriptions</li>
+      <li>lightweight</li>
     </ul>
-    <h3 className="disadvatages">Disadvantages</h3>
+    <h3 className="disadvantages">Disadvantages</h3>
     <ul>
-      <li>disadvantage 1</li>
+      <li>some migration effort required if migrating from Redux Form</li>
     </ul>
     <h3>When should you use this</h3>
     <ul>
-      <li>reason to use</li>
+      <li>when a controlled component becomes too verbose or too cumbersome</li>
+      <li>when you need good validation support</li>
+      <li>when optimizing for speed is critical (use subscriptions)</li>
     </ul>
   </div>
 );
