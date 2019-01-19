b5a7dacb967858d874a89b5d6c092cb92f25dcc5
Etienne Dilocker <etienne@dilocker.de>
2019-01-19 12:06:11 +0100

    implement redux form solution
    this corresponds to part 3 of the video series

--- src/ReduxForm.js
+++ src/ReduxForm.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import Input from './Input';
 import {Link} from 'react-router-dom';
+import {reduxForm, Field} from 'redux-form';
 
-const ReduxForm = () => (
+const renderInput = ({input, meta}) => (
+  <Input {...input} type="text" errorMessage={meta.touched && meta.error} />
+);
+
+const onSubmit = values => {
+  alert(JSON.stringify(values));
+};
+
+const required = v => {
+  if (!v || v === '') {
+    return 'This field is required';
+  }
+
+  return undefined;
+};
+
+const allowedNames = v => {
+  if (v === 'forbidden name') {
+    return '"forbidden name" is not a valid customer id';
+  }
+
+  return undefined;
+};
+
+const ReduxForm = ({handleSubmit, valid}) => (
   <div>
     <h2>Redux Form</h2>
-    <form>
-      <Input
-        type="text"
-        label="Customer ID"
-        id="customer-id"
+    <form onSubmit={handleSubmit}>
+      <Field
         name="customer-id"
+        component={renderInput}
+        validate={[required, allowedNames]}
       />
-      <button type="submit">Submit</button>
+      <button disabled={!valid} type="submit">
+        Submit
+      </button>
     </form>
     <Link to="summary">Conclusions</Link>
   </div>
 );
 
-export default ReduxForm;
+export default reduxForm({
+  form: 'my-customer-registration-form',
+  onSubmit,
+})(ReduxForm);
--- src/summary/ReduxForm.js
+++ src/summary/ReduxForm.js
@@ -3,17 +3,28 @@ import React from 'react';
 const Summary = () => (
   <div>
     <h2>Redux Form - Conclusions</h2>
-    <h3 className="advatages">Advantages</h3>
+    <h3 className="advantages">Advantages</h3>
     <ul>
-      <li>advantage 1</li>
+      <li>much less repition</li>
+      <li>easy way to manage validators</li>
+      <li>a lot of interesting "state" by default (valid, touched, etc.)</li>
+      <li>good API</li>
     </ul>
-    <h3 className="disadvatages">Disadvantages</h3>
+    <h3 className="disadvantages">Disadvantages</h3>
     <ul>
-      <li>disadvantage 1</li>
+      <li>unnecessary dependency on redux</li>
+      <li>
+        redux (i.e. app-wide centralized state) is actually the wrong place to
+        store local state
+      </li>
     </ul>
     <h3>When should you use this</h3>
     <ul>
-      <li>reason to use</li>
+      <li>if possible, use FinalForm instead</li>
+      <li>
+        if for whatever reason you can't don't want to use FinalForm, complex
+        forms requiring plenty of validation is a good use-case
+      </li>
     </ul>
   </div>
 );
